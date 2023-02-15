5dc5c5d13fd440d28cf3747df483cdf54505e3c6
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2023-02-15 15:50:34 +0100

    Work with models and admin panel

--- service/products/admin.py
+++ service/products/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.forms import TextInput
+from django.db import models
 
 from .models import Item, Order
 
-admin.site.register(Item)
-admin.site.register(Order)
+admin.site.site_header = "Game Store administration"
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_price', 'currency')
+    list_editable = ('currency', )
+
+    class Media:
+        css = {
+            'all': ('css/admin-panel-styles.css',)
+        }
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
--- service/products/models.py
+++ service/products/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Item(models.Model):
     """Product item model"""
+
+    class ItemCurrency(models.TextChoices):
+        USD = 'USD',
+        Euro = 'Euro',
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField(default=0)  # in cents
+    currency = models.CharField(max_length=20, choices=ItemCurrency.choices, default=ItemCurrency.USD)
 
     def __str__(self):
         return self.name
