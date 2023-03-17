46ae785f0e8b43b57f47169a6b91281a6f781238
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2023-03-17 17:12:31 +0100

    Refactoring some js files and the function of opening a game screenshot in full screen on the game page has been developed

--- service/static/js/changeItemFavoriteIcon.js
+++
@@ -1,9 +0,0 @@
-let favoriteImg = document.getElementById("favorite-img");
-
-favoriteImg.addEventListener("mouseover", () => {
-    favoriteImg.src = 'https://cdn-icons-png.flaticon.com/512/535/535183.png'
-})
-
-favoriteImg.addEventListener("mouseout", () => {
-    favoriteImg.src = 'https://cdn-icons-png.flaticon.com/512/535/535285.png'
-})
---
+++ service/static/js/ui-components.js
@@ -0,0 +1,35 @@
+function changeItemFavoriteIcon() {
+    let favoriteImg = document.getElementById("favorite-img");
+
+    favoriteImg.addEventListener("mouseover", () => {
+        favoriteImg.src = 'https://cdn-icons-png.flaticon.com/512/535/535183.png'
+    })
+
+    favoriteImg.addEventListener("mouseout", () => {
+        favoriteImg.src = 'https://cdn-icons-png.flaticon.com/512/535/535285.png'
+    })
+}
+
+changeItemFavoriteIcon();
+
+function showPopup() {
+    const popupImg = document.querySelectorAll('.popup')
+    const screenShot = document.querySelectorAll('.screenshot')
+    const closePopup = document.getElementById("popup-close")
+
+
+    popupImg.forEach((popup) => {
+        popup.addEventListener('click', () => {
+            popup.classList.toggle('popup-open')
+            closePopup.classList.toggle('hide')
+            document.body.classList.toggle("stop-scrolling");
+        })
+
+        closePopup.addEventListener('click', () => {
+            popup.classList.remove('popup-close')
+            console.log('закрыто')
+        })
+    })
+}
+
+showPopup();
